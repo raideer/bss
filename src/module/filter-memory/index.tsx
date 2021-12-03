c48976d94adac67b946b0d29c220953c440e238d
raideer <hi@kristofers.dev>
2021-12-03 22:54:23 +0200

    Added filter memory setting

--- src/module/filter-memory/index.tsx
+++ src/module/filter-memory/index.tsx
@@ -1,10 +1,20 @@
-import { getItem, setItem } from "module/settings/storage";
+import { getItem, registerSetting, setItem, SettingCategory, SettingValueType } from "module/settings/storage";
 import { render } from "preact/compat";
 import { dom } from "util/dom";
 import { whenLoaded } from "util/lifecycle";
 import { FilterMemory } from "./FilterMemory";
 
+export const SETTING_ENABLED = 'preview-enabled'
 export const STORAGE_MEMORY = 'ssplus-filter-mem'
+
+registerSetting({
+  id: SETTING_ENABLED,
+  type: SettingValueType.Checkbox,
+  defaultValue: 'true',
+  menu: SettingCategory.AdList,
+  title: 'Filtru atmiņa',
+  description: 'Saglabā un uzstādi filtrus ar 1 klikšķi'
+})
 
 export function getSavedFilters() {
   const data = getItem(STORAGE_MEMORY)
@@ -21,6 +31,8 @@ export function saveFilters(filters: {[key: string]: any}) {
 }
 
 whenLoaded(() => {
+  if (getItem(SETTING_ENABLED) !== 'true') return
+
   const filterForm = document.querySelector('#filter_frm')
 
   if (!filterForm) {
