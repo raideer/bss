7ab67354650c62db29c728e3fab4947e4cb623b2
raideer <hi@kristofers.dev>
2023-06-25 14:48:08 +0300

    fix: build tools

--- webpack.chrome.config.js
+++ webpack.chrome.config.js
@@ -1,3 +1,15 @@
 const common = require('./webpack.common')
+const ZipPlugin = require('zip-webpack-plugin')
 
-module.exports = common('chrome')
+const config = common('chrome')
+
+config.plugins.push(
+  new ZipPlugin({
+    filename: `bss-chrome.zip`,
+    exclude: [
+      'node_modules'
+    ]
+  })
+)
+
+module.exports = config
